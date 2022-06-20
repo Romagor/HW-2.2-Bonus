6c00ea1a59069cf519213b5c996bbf75df375b48
RomaGor <goryachevskii60@gmail.com>
2022-06-20 11:05:26 +0300

    Bonus

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int initialAccount = 100;
+        int depositAmount = 1100;
+
+        int finalAccount;
+        int bonus;
+
+        if (depositAmount >= 1100) {
+            bonus = depositAmount / 100;
+            finalAccount = initialAccount + depositAmount + bonus;
+        } else {
+            bonus = 0;
+            finalAccount = initialAccount + depositAmount;
+        }
+        System.out.println((bonus) + " бонус");
+        System.out.println((finalAccount) + " итоговый счёт");
+    }
 }
